145e42962e579ece8a2d82bb228a2714063be5d6
kojonek2 <kojonek2@wp.pl>
2017-03-15 12:19:56 +0100

    saving and loading is now working

--- src/kojonek2/tictactoe/common/MyFileChooser.java
+++ src/kojonek2/tictactoe/common/MyFileChooser.java
@@ -11,46 +11,69 @@ public class MyFileChooser extends JFileChooser {
 
 	@Override
 	public void approveSelection() {
-		if(getDialogType() == SAVE_DIALOG) {
+		if (getDialogType() == SAVE_DIALOG) {
 			saveDialogApprove();
-		} else if(getDialogType() == OPEN_DIALOG) {
+		} else if (getDialogType() == OPEN_DIALOG) {
 			openDialogApprove();
 		} else {
 			super.approveSelection();
 		}
 	}
-	
+
 	private void saveDialogApprove() {
 		File file = getSelectedFile();
-		
-		String extension = getExtension(file);
-		if(extension == null) {
-			String path = file.getAbsolutePath();
-			setSelectedFile(new File(path + ".json"));
-		} else if(!extension.equals("json")) {
-			Toolkit.getDefaultToolkit().beep();
-			JOptionPane.showMessageDialog(this, "You need to choose file with extension .json", "Warning", JOptionPane.WARNING_MESSAGE);
+
+		if (!checkExtension(file)) {
 			return;
 		}
 
 		file = getSelectedFile();
-		if(file.exists()) {
+		if (file.exists()) {
 			int resultCode = JOptionPane.showConfirmDialog(this, "Would you like to override this file?");
-			if(resultCode != JOptionPane.YES_OPTION) {
+			if (resultCode != JOptionPane.YES_OPTION) {
 				return;
 			}
 		}
-		
+
 		super.approveSelection();
 	}
-	
+
 	private void openDialogApprove() {
-		
+		File file = getSelectedFile();
+
+		if (!checkExtension(file)) {
+			return;
+		}
+
+		file = getSelectedFile();
+		if (!file.exists()) {
+			Toolkit.getDefaultToolkit().beep();
+			JOptionPane.showMessageDialog(this, "You need to choose existing file to load!", "Warning",
+					JOptionPane.WARNING_MESSAGE);
+			return;
+		}
+
+		super.approveSelection();
 	}
-	
+
+	private boolean checkExtension(File file) {
+		String extension = getExtension(file);
+		if (extension == null) {
+			String path = file.getAbsolutePath();
+			setSelectedFile(new File(path + ".json"));
+			return true;
+		} else if (!extension.equals("json")) {
+			Toolkit.getDefaultToolkit().beep();
+			JOptionPane.showMessageDialog(this, "You need to choose file with extension .json!", "Warning",
+					JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		return true;
+	}
+
 	private String getExtension(File file) {
 		String[] splitedName = file.getName().split("\\.");
-		if(splitedName.length > 1) {
+		if (splitedName.length > 1) {
 			return splitedName[splitedName.length - 1];
 		}
 		return null;
